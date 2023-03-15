2b98988b2666a8c2ad1eb35e7a89022e4eb38325
Petr Krejcik <p.krejcik@stuart.com>
2023-03-15 23:51:48 +0100

    fix

--- src/routes/week/[weekId]/index.tsx
+++ src/routes/week/[weekId]/index.tsx
@@ -1,17 +1,17 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead} from "@builder.io/qwik-city";
+import type { DocumentHead } from "@builder.io/qwik-city";
 import { useLocation } from "@builder.io/qwik-city";
 import WeekPlan from "~/components/WeekPlanPage/WeekPlanPage";
 
 export default component$(() => {
   const location = useLocation();
-  const weekId = parseInt(location.params.weekId)
-  if (isNaN(weekId)) {
-    throw new Error('Incorrent week')
-  }
-  return <>
-  <WeekPlan weekId={weekId}/>
-  </>
+  const weekId = location.params.weekId;
+  
+  return (
+    <>
+      <WeekPlan weekId={weekId} />
+    </>
+  );
 });
 
 export const head: DocumentHead = {
