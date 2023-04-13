14395f597f828d06763495da16f7f2e2a5b3529c
Petr Krejcik <p.krejcik@stuart.com>
2023-04-14 00:08:27 +0200

    fix

--- src/lib/user/refreshCustomToken.ts
+++ src/lib/user/refreshCustomToken.ts
@@ -2,7 +2,7 @@ import { User } from "firebase/auth";
 
 export default async (user: User)=>{
   const idToken = await user.getIdToken()
-  const response = await fetch(`http://localhost:3001/api/session?token=${idToken}`);
+  const response = await fetch(`/api/session?token=${idToken}`);
   const result = await response.json();
   console.log('🛎 ', 'result', result);
 }
